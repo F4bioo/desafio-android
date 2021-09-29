d6acfaf8f38965ad6fe827de5a86e4a8483661e4
F4bioo <costa.fbo@gmail.com>
2021-09-28 22:32:15 -0300

    Create main view model

---
+++ app/src/main/java/com/picpay/desafio/android/ui/viewmodel/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.picpay.desafio.android.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.picpay.desafio.android.data.api.DataState
+import com.picpay.desafio.android.data.model.User
+import com.picpay.desafio.android.data.usecase.GetUsers
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel
+@Inject
+constructor(
+    private val getUsers: GetUsers
+) : ViewModel() {
+    private val _getUsersEvent = MutableLiveData<DataState<List<User>>?>()
+    val getUsersEvent: LiveData<DataState<List<User>>?>
+        get() = _getUsersEvent
+
+    private fun getUsers() {
+        viewModelScope.launch {
+            _getUsersEvent.value = getUsers.invoke()
+        }
+    }
+}
